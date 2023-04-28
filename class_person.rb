27fe443e68ad205016706f7bf0c6da8c97247ca9
Kwan Tan <tkwanxuan@gmail.com>
2023-04-28 15:05:56 +0800

    commit 2

--- class_person.rb
+++ class_person.rb
@@ -1,26 +1,38 @@
-class Person
-    attr_accessor :name
+PEOPLE = []
 
+count = PEOPLE.length
+class Person
+
+    attr_accessor :name, :age
     def initialize(name)
          @name = name
+         @age = age
     end
 
 end
-#empty array
 
 
 #define a method that ask user to put their name and then add it to the array
 def add_person
-    people = []
     puts "Hi, what is your name?"
     @name = gets.chomp
     person = Person.new(@name)
-    people.push(person)
+    PEOPLE.push(person)
+end
+
+def age_user
+    puts "How old are you?"
+    @age = gets.chomp
+    puts "Hi #{@name.capitalize}, you are #{@age} years old."
 end
 
 def greet_user
     puts "Hi #{@name.capitalize}, how are you?"
+
 end
+
 
 add_person
 greet_user
+age_user
+puts "There are #{PEOPLE.length} people in the array."
